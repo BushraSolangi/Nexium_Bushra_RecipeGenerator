beb969f5bc4342d9eae6932c6edd071aac70d3ae
BushraSolangi <bushrabia000@gmail.com>
2025-08-01 20:10:27 +0500

    updated

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,16 +1,16 @@
 // app/providers.tsx
 "use client";
 
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
 
 export function SupabaseProvider({ children }: { children: React.ReactNode }) {
   const [supabaseClient] = useState(() =>
-    createBrowserClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
+    createBrowserSupabaseClient({
+      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    })
   );
 
   return (
